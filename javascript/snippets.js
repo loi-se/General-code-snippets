2264ed194c6d105471d063a062b01011e8a31ba5
loi-se <rolandmeijerink@gmail.com>
2016-01-03 13:11:47 +0100

    Update snippets.js

--- javascript/snippets.js
+++ javascript/snippets.js
@@ -71,6 +71,13 @@ for (element in object) {
   // …
 }
 
+var sum = 0;
+var obj = {prop1: 5, prop2: 13, prop3: 8};
+
+for each (var item in obj) {
+  sum += item;
+}
+
 //--------------Switch
 switch(i) {
 case 1:
