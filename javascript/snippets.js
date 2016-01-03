13b7fecfbe6e86f5e08b98e91cf27f3682615688
loi-se <rolandmeijerink@gmail.com>
2016-01-03 14:05:52 +0100

    Update snippets.js

--- javascript/snippets.js
+++ javascript/snippets.js
@@ -79,16 +79,28 @@ for each (var item in obj) {
 }
 
 //--------------Switch
-switch(i) {
-case 1:
-  // …
-  break;
-case 2:
-  // …
-  break;
-default:
-  // …
-  break;
+switch (new Date().getDay()) {
+    case 0:
+        day = "Sunday";
+        break;
+    case 1:
+        day = "Monday";
+        break;
+    case 2:
+        day = "Tuesday";
+        break;
+    case 3:
+        day = "Wednesday";
+        break;
+    case 4:
+        day = "Thursday";
+        break;
+    case 5:
+        day = "Friday";
+        break;
+    case 6:
+        day = "Saturday";
+        break;
 }
 
 //--------------With
