13c72ee3b677cc00f826cf4bd52e137fe52e9101
loi-se <rolandmeijerink@gmail.com>
2016-01-03 14:08:56 +0100

    Update snippets.cs

--- Csharp/snippets.cs
+++ Csharp/snippets.cs
@@ -1,1 +1,50 @@
+// -----------------------1. Control structures
 
+//-------------If
+
+
+//----------- If else
+
+
+//------------- While loop
+
+
+//-----------do while loop
+
+//-------------for loop
+
+
+//-------------for each loop
+
+
+//--------------Switch
+
+
+//--------------With
+
+
+//-------------------------------- 2. Data Structures:
+
+// -----------Array
+
+
+// add value
+
+
+// loop trough it
+
+
+
+//---------- Stack
+
+
+//---------- Queue
+
+
+
+//--------Dictionairy
+
+// ------Hashtable
+
+
+// Iterate trough it:
