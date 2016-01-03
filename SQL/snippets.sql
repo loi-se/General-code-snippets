4a6332a32a629f2249f274ea23c4f6b69153104c
loi-se <rolandmeijerink@gmail.com>
2016-01-03 14:21:32 +0100

    Update snippets.sql

--- SQL/snippets.sql
+++ SQL/snippets.sql
@@ -1,1 +1,22 @@
 
+/*------------------- 1. General examples:
+
+
+
+/* ------inner join */
+SELECT
+    `city`.`Name`
+    , `country`.`Continent`
+    , `countrylanguage`.`Language`
+FROM
+    `world`.`city`
+    INNER JOIN `world`.`country` 
+        ON (`city`.`CountryCode` = `country`.`Code`)
+    INNER JOIN `world`.`countrylanguage` 
+        ON (`countrylanguage`.`CountryCode` = `country`.`Code`)
+WHERE (`city`.`Name` = 'Den-Haag');
+
+
+/* ---------------------2. 2jours database */
+
+
