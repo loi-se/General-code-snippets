cdf85ce8c650b05cdfbb176253db77476c2eb547
loi-se <rolandmeijerink@gmail.com>
2016-01-06 09:38:14 +0100

    Update snippets.cs

--- Csharp/snippets.cs
+++ Csharp/snippets.cs
@@ -48,3 +48,19 @@
 
 
 // Iterate trough it:
+
+
+// Build a HTML dropdownbox with C#:
+ string html = "";
+            html += "<select name='" + name + "' style='width: 100%'>";
+            html += "<option value='5'" + (value == 5 || toonUitgebreidePrintopties ? " selected" : "") + ">8 weken</option>";
+            html += "<option value='4'" + (value == 3 && !toonUitgebreidePrintopties ? " selected" : "") + disabled + ">12 weken" + nb + "</option>";
+            html += "<option value='3'" + (value == 3 && !toonUitgebreidePrintopties ? " selected" : "") + disabled + ">18 weken" + nb + "</option>";
+            html += "<option value='2'" + (value == 2 && !toonUitgebreidePrintopties ? " selected" : "") + disabled + ">28 weken" + nb + "</option>";
+            html += "<option value='1'" + (value == 1 && !toonUitgebreidePrintopties ? " selected" : "") + disabled + ">38 weken" + nb + "</option>";
+            html += "<option value='6'" + (value == 6 && !toonUitgebreidePrintopties ? " selected" : "") + disabled + ">50 weken" + nb + "</option>";
+            html += "</select>";
+            return html;
+
+
+
