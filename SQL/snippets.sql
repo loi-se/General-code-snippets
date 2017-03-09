c9352f3f1099d44a51957b7ca5f7815e058c369d
loi-se <rolandmeijerink@gmail.com>
2017-03-09 10:56:13 +0100

    Update snippets.sql

--- SQL/snippets.sql
+++ SQL/snippets.sql
@@ -1,6 +1,12 @@
 
 /*------------------- 1. General examples:
 
+/*--------------1.1 Joins:
+INNER JOIN: Returns all rows when there is at least one match in BOTH tables
+
+LEFT JOIN: Return all rows from the left table, and the matched rows from the right table
+RIGHT JOIN: Return all rows from the right table, and the matched rows from the left table
+FULL JOIN: Return all rows when there is a match in ONE of the tables
 
 
 /* ------inner join */
@@ -9,7 +15,26 @@ FROM Orders
 INNER JOIN Customers
 ON Orders.CustomerID = Customers.CustomerID;
 
+/*-------Left join */
+SELECT Customers.CustomerName, Orders.OrderID
+FROM Customers
+LEFT JOIN Orders
+ON Customers.CustomerID=Orders.CustomerID
+ORDER BY Customers.CustomerName;
 
+/*------Right join */
+SELECT Orders.OrderID, Employees.FirstName
+FROM Orders
+RIGHT JOIN Employees
+ON Orders.EmployeeID=Employees.EmployeeID
+ORDER BY Orders.OrderID;
+
+/*------Outer join */
+SELECT Customers.CustomerName, Orders.OrderID
+FROM Customers
+FULL OUTER JOIN Orders
+ON Customers.CustomerID=Orders.CustomerID
+ORDER BY Customers.CustomerName;
 
 /* ---------------------2.  */
 
